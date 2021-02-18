e8ca737b7535ae246bfaed5d2bda00418ef3d89d
JFAexe <jfa35ru@gmail.com>
2021-02-18 23:43:47 +0300

    2021.2.18.5
    Upload

---
+++ README.md
@@ -0,0 +1,115 @@
+<div><br><img width="40%" src="Vectr2.svg?sanitize=true"></div>
+
+Very simple and far from perfect 2D vector lua library.
+
+## Usage
+```lua
+local Vectr2 = require('vectr2')
+```
+
+## Vectr2 functions
+```lua
+Vectr2.zero              --> Vectr2(0, 0)
+Vectr2.one               --> Vectr2(1, 1)
+Vectr2.up                --> Vectr2(0, -1)
+Vectr2.down              --> Vectr2(0, 1)
+Vectr2.left              --> Vectr2(-1, 0)
+Vectr2.right             --> Vectr2(1, 0)
+Vectr2.isVectr2(t)       --> true if t is table and has vectr2 __type
+Vectr2.isVectr2ByMeta(t) --> true if t's metatable is same as Vectr2's metatable
+Vectr2._meta             --> Vectr2 Metatable if needed
+```
+
+## New
+```lua
+Vectr2(x, y) --> Vectr2(x, y)
+Vectr2(x, _) --> Vectr2(x, x)
+Vectr2(_, _) --> Vectr2(0, 0)
+```
+
+## Adding
+```lua
+vec1 + vec2 --> Vectr2(vec1.x + vec2.x, vec1.y + vec2.y)
+vec1 + x    --> Vectr2(vec1.x + x, vec1.y + x)
+x + vec1    --> Vectr2(x + vec1.x, x + vec1.y)
+```
+
+## Subbing
+```lua
+vec1 - vec2 --> Vectr2(vec1.x - vec2.x, vec1.y - vec2.y)
+vec1 - x    --> Vectr2(vec1.x - x, vec1.y - x)
+x - vec1    --> Vectr2(x - vec1.x, x - vec1.y)
+```
+
+## Multiplying
+```lua
+vec1 * vec2 --> Vectr2(vec1.x * vec2.x, vec1.y * vec2.y)
+vec1 * x    --> Vectr2(vec1.x * x, vec1.y * x)
+x * vec1    --> Vectr2(x * vec1.x, x * vec1.y)
+```
+
+## Dividing
+```lua
+vec1 / vec2 --> Vectr2(vec1.x / vec2.x, vec1.y / vec2.y)
+vec1 / x    --> Vectr2(vec1.x / x, vec1.y / x)
+x / vec1    --> Vectr2(x / vec1.x, x / vec1.y)
+```
+
+## Power of
+```lua
+vec1 ^ vec2 --> Vectr2(vec1.x ^ vec2.x, vec1.y ^ vec2.y)
+vec1 ^ x    --> Vectr2(vec1.x ^ x, vec1.y ^ x)
+x ^ vec1    --> Vectr2(x ^ vec1.x, x ^ vec1.y)
+```
+
+## Module of
+```lua
+vec1 % vec2 --> Vectr2(vec1.x % vec2.x, vec1.y % vec2.y)
+vec1 % x    --> Vectr2(vec1.x % x, vec1.y % x)
+x % vec1    --> Vectr2(x % vec1.x, x % vec1.y)
+```
+
+## Comparing vectors
+```lua
+vec1 < vec2  --> vec1.x < vec2.x and vec1.y < vec2.y
+vec1 <= vec2 --> vec1.x <= vec2.x and vec1.y <= vec2.y
+vec1 == vec2 --> vec1.x == vec2.x and vec1.y == vec2.y
+```
+
+## Convert to string
+```lua
+tostring(vec1) --> '[x: vec1.x, y: vec1.y]'
+text .. vec1   --> 'text [x: vec1.x, y: vec1.y]'
+```
+
+## Other operations
+```lua
+-vec1 --> negative vec1
+#vec1 --> length of vec1 (lua 5.2+ I guess)
+```
+
+## Functions
+```lua
+vec1:copy()        --> copy of vec1
+vec1:set(x, y)     --> sets x and y of vec1 (nil values will be ignored and not sat)
+vec1:unpack()      --> vec1.x and vec1.y
+vec1:floor()       --> floors vec1
+vec1:ceil()        --> ceils vec1
+vec1:abs()         --> abs of vec1
+vec1:angle()       --> vec1 angle
+vec1:scale(x)      --> scales vec1
+vec1:cross(vec2)   --> cross product of vec1 and vec2
+vec1:dot(vec2)     --> dot product of vec1 and vec2
+vec1:perpDot(vec2) --> perp dot product of vec1 and vec2
+vec1:lenSq()       --> squared length of vec1
+vec1:len()         --> length of vec1
+vec1:dist(vec2)    --> distance between vec1 and vec2
+vec1:mid(vec2)     --> middle of vec1 and vec2
+vec1:normalize()   --> normalized copy of vec1
+vec1:normal()      --> normal of vec1
+```
+
+Currently no lua 5.3+ functions.
+
+## License
+[MIT](LICENSE)
---
+++ vectr2.lua
@@ -0,0 +1,241 @@
+-------------------------------------------------------------------------------
+-- Name:        Vectr2.lua
+-- Description: Very simple Lua 2D vector library
+-- Author:      Alexandr 'JFAexe' Konichenko
+-- Version:     2021.2.18.5
+-------------------------------------------------------------------------------
+
+local setmetatable, getmetatable, assert, type, math = setmetatable, getmetatable, assert, type, math
+local abs, ceil, floor, atan2 = math.abs, math.ceil, math.floor, math.atan2
+
+local Vectr2, Vectr2Meta = {}, {}
+
+Vectr2._meta = Vectr2Meta
+Vectr2._type = 'vectr2'
+
+local function isnumber(n)
+    return type(n) == 'number'
+end
+
+local function istable(t)
+    return type(t) == 'table'
+end
+
+local function nassert(t, a)
+    assert(isnumber(t), '[Vectr2] Wrong type of ' .. a .. ' argument. Expected number, got ' .. type(t) .. '.')
+end
+
+local function vectrassert(a)
+    assert(Vectr2.isVectr2(a), '[Vectr2] Wrong type of vectr2 argument. Expected table (vectr2), got ' .. type(a) .. '.')
+end
+
+local function vectrnumbassert(a, b)
+    assert(isnumber(a) or Vectr2.isVectr2(a), '[Vectr2] Wrong type of first argument. Expected number or table (vectr2), got ' .. type(a) .. '.')
+    assert(isnumber(b) or Vectr2.isVectr2(b), '[Vectr2] Wrong type of second argument. Expected number or table (vectr2), got ' .. type(b) .. '.')
+end
+
+local function zeroassert(a)
+    assert(a ~= 0, '[Vectr2] Division by zero.')
+end
+
+function Vectr2.isVectr2(t)
+    return istable(t) and t['__type'] == Vectr2._type
+end
+
+function Vectr2.isVectr2ByMeta(t)
+    return getmetatable(t) == Vectr2._meta
+end
+
+Vectr2Meta.__index = Vectr2Meta
+
+function Vectr2Meta:__call(x, y)
+    nassert(x, 'x')
+    nassert(y, 'y')
+
+    local _vec = {
+        x = x or 0,
+        y = y or x
+    }
+
+    _vec['__type'] = Vectr2._type
+
+    return setmetatable(_vec, Vectr2Meta)
+end
+
+function Vectr2Meta:__add(v)
+    vectrnumbassert(self, v)
+
+    local sn, vn = isnumber(self), isnumber(v)
+
+    return Vectr2((sn and self or self.x) + (vn and v or v.x), (sn and self or self.y) + (vn and v or v.y))
+end
+
+function Vectr2Meta:__sub(v)
+    vectrnumbassert(self, v)
+
+    local sn, vn = isnumber(self), isnumber(v)
+
+    return Vectr2((sn and self or self.x) - (vn and v or v.x), (sn and self or self.y) - (vn and v or v.y))
+end
+
+function Vectr2Meta:__mul(v)
+    vectrnumbassert(self, v)
+
+    local sn, vn = isnumber(self), isnumber(v)
+
+    return Vectr2((sn and self or self.x) * (vn and v or v.x), (sn and self or self.y) * (vn and v or v.y))
+end
+
+function Vectr2Meta:__div(v)
+    vectrnumbassert(self, v)
+    zeroassert(v)
+
+    local sn, vn = isnumber(self), isnumber(v)
+
+    return Vectr2((sn and self or self.x) / (vn and v or v.x), (sn and self or self.y) / (vn and v or v.y))
+end
+
+function Vectr2Meta:__pow(v)
+    vectrnumbassert(self, v)
+
+    local sn, vn = isnumber(self), isnumber(v)
+
+    return Vectr2((sn and self or self.x) ^ (vn and v or v.x), (sn and self or self.y) ^ (vn and v or v.y))
+end
+
+function Vectr2Meta:__mod(v)
+    vectrnumbassert(self, v)
+    zeroassert(v)
+
+    local sn, vn = isnumber(self), isnumber(v)
+
+    return Vectr2((sn and self or self.x) % (vn and v or v.x), (sn and self or self.y) % (vn and v or v.y))
+end
+
+function Vectr2Meta:__unm()
+    return Vectr2(-self.x, -self.y)
+end
+
+function Vectr2Meta:__len()
+    return self:len()
+end
+
+function Vectr2Meta:__eq(v)
+    vectrassert(v, 'vectr2')
+
+    return self.x == v.x and self.y == v.y
+end
+
+function Vectr2Meta:__lt(v)
+    vectrassert(v)
+
+    return self.x < v.x and self.y < v.y
+end
+
+function Vectr2Meta:__le(v)
+    vectrassert(v)
+
+    return self.x <= v.x and self.y <= v.y
+end
+
+function Vectr2Meta:__tostring()
+    return '[x: ' .. tostring(self.x) .. ', y: ' .. tostring(self.y) .. ']'
+end
+
+function Vectr2Meta:__concat(v)
+    return tostring(self) .. ' ' .. tostring(v)
+end
+
+function Vectr2Meta:copy()
+    return self
+end
+
+function Vectr2Meta:set(x, y)
+    nassert(x, 'x')
+    nassert(y, 'y')
+
+    self.x, self.y = x and x or self.x, y and y or self.y
+end
+
+function Vectr2Meta:unpack()
+    return self.x, self.y
+end
+
+function Vectr2Meta:floor()
+    return Vectr2(floor(self.x), floor(self.y))
+end
+
+function Vectr2Meta:ceil()
+    return Vectr2(ceil(self.x), ceil(self.y))
+end
+
+function Vectr2Meta:abs()
+    return Vectr2(abs(self.x), abs(self.y))
+end
+
+function Vectr2Meta:angle()
+    return atan2(self.y, self.x)
+end
+
+function Vectr2Meta:scale(s)
+    nassert(s, 'number')
+
+    return Vectr2(self.x * s, self.y * s)
+end
+
+function Vectr2Meta:cross(v)
+    vectrassert(v)
+
+    return self.x * v.y - self.y * v.x
+end
+
+function Vectr2Meta:dot(v)
+    vectrassert(v)
+
+    return self.x * v.x + self.y * v.y
+end
+
+function Vectr2Meta:perpDot(v)
+    vectrassert(v)
+
+    return self.x * v.x - self.y * v.y
+end
+
+function Vectr2Meta:lenSq()
+    return self.x ^ 2 + self.y ^ 2
+end
+
+function Vectr2Meta:len()
+    return self:lenSq() ^ 0.5
+end
+
+function Vectr2Meta:dist(v)
+    vectrassert(v)
+
+    return (v - self):len()
+end
+
+function Vectr2Meta:mid(v)
+    vectrassert(v)
+
+    return self + (v - self) * 0.5
+end
+
+function Vectr2Meta:normalize()
+    return self / self:len()
+end
+
+function Vectr2Meta:normal()
+    return Vectr2(self.y, -self.x)
+end
+
+setmetatable(Vectr2, Vectr2Meta)
+
+Vectr2.zero  = Vectr2(0, 0)
+Vectr2.one   = Vectr2(1, 1)
+Vectr2.up    = Vectr2(0, -1)
+Vectr2.down  = Vectr2(0, 1)
+Vectr2.left  = Vectr2(-1, 0)
+Vectr2.right = Vectr2(1, 0)
+
+return Vectr2
